85c609f0baf09edfa807bf2eb0ab68fd800a540f
Dima Zubkov <dima5447298@gmail.com>
2021-05-18 20:45:55 +0300

    fullcalendar RU

--- pages/fullcalendar.vue
+++ pages/fullcalendar.vue
@@ -3,49 +3,46 @@ import FullCalendar from '@fullcalendar/vue'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
+import ruLocale from '@fullcalendar/core/locales/ru'
 // import { INITIAL_EVENTS, createEventId } from './event-utils'
 
-
 let eventGuid = 0
-let todayStr = new Date().toISOString().replace(/T.*$/, '') // YYYY-MM-DD of today
+const todayStr = new Date().toISOString().replace(/T.*$/, '') // YYYY-MM-DD of today
 
 const INITIAL_EVENTS = [
   {
     id: createEventId(),
     title: 'All-day event',
-    start: todayStr
+    start: todayStr,
   },
   {
     id: createEventId(),
     title: 'Timed event',
-    start: todayStr + 'T12:00:00'
-  }
+    start: todayStr + 'T12:00:00',
+  },
 ]
 
 function createEventId() {
   return String(eventGuid++)
 }
 
-
-
-
-
 export default {
   components: {
-    FullCalendar // make the <FullCalendar> tag available
+    FullCalendar, // make the <FullCalendar> tag available
   },
-  data: function() {
+  data() {
     return {
       calendarOptions: {
         plugins: [
           dayGridPlugin,
           timeGridPlugin,
-          interactionPlugin // needed for dateClick
+          interactionPlugin, // needed for dateClick
         ],
+        locale: ruLocale,
         headerToolbar: {
           left: 'prev,next today',
           center: 'title',
-          right: 'dayGridMonth,timeGridWeek,timeGridDay'
+          right: 'dayGridMonth,timeGridWeek,timeGridDay',
         },
         initialView: 'dayGridMonth',
         initialEvents: INITIAL_EVENTS, // alternatively, use the `events` setting to fetch from a feed
@@ -56,14 +53,15 @@ export default {
         weekends: true,
         select: this.handleDateSelect,
         eventClick: this.handleEventClick,
-        eventsSet: this.handleEvents
+        eventsSet: this.handleEvents,
+
         /* you can update a remote database when these fire:
         eventAdd:
         eventChange:
         eventRemove:
         */
       },
-      currentEvents: []
+      currentEvents: [],
     }
   },
   methods: {
@@ -71,8 +69,8 @@ export default {
       this.calendarOptions.weekends = !this.calendarOptions.weekends // update a property
     },
     handleDateSelect(selectInfo) {
-      let title = prompt('Please enter a new title for your event')
-      let calendarApi = selectInfo.view.calendar
+      const title = prompt('Please enter a new title for your event')
+      const calendarApi = selectInfo.view.calendar
       calendarApi.unselect() // clear date selection
       if (title) {
         calendarApi.addEvent({
@@ -80,26 +78,30 @@ export default {
           title,
           start: selectInfo.startStr,
           end: selectInfo.endStr,
-          allDay: selectInfo.allDay
+          allDay: selectInfo.allDay,
         })
       }
     },
     handleEventClick(clickInfo) {
-      if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+      if (
+        confirm(
+          `Are you sure you want to delete the event '${clickInfo.event.title}'`
+        )
+      ) {
         clickInfo.event.remove()
       }
     },
     handleEvents(events) {
       this.currentEvents = events
-    }
-  }
+    },
+  },
 }
 </script>
 
 <template>
-  <div class='demo-app'>
-    <div class='demo-app-sidebar'>
-      <div class='demo-app-sidebar-section'>
+  <div class="demo-app">
+    <div class="demo-app-sidebar">
+      <div class="demo-app-sidebar-section">
         <h2>Instructions</h2>
         <ul>
           <li>Select dates and you will be prompted to create a new event</li>
@@ -107,32 +109,33 @@ export default {
           <li>Click an event to delete it</li>
         </ul>
       </div>
-      <div class='demo-app-sidebar-section'>
+      <div class="demo-app-sidebar-section">
         <label>
           <input
-            type='checkbox'
-            :checked='calendarOptions.weekends'
-            @change='handleWeekendsToggle'
+            type="checkbox"
+            :checked="calendarOptions.weekends"
+            @change="handleWeekendsToggle"
           />
           toggle weekends
         </label>
       </div>
-      <div class='demo-app-sidebar-section'>
+      <div class="demo-app-sidebar-section">
         <h2>All Events ({{ currentEvents.length }})</h2>
         <ul>
-          <li v-for='event in currentEvents' :key='event.id'>
+          <li v-for="event in currentEvents" :key="event.id">
             <b>{{ event.startStr }}</b>
             <i>{{ event.title }}</i>
           </li>
         </ul>
       </div>
     </div>
-    <div class='demo-app-main'>
+    <div class="demo-app-main">
       <FullCalendar
-        class='demo-app-calendar'
-        :options='calendarOptions'
+        class="demo-app-calendar"
+        :options="calendarOptions"
+        locale="ru"
       >
-        <template v-slot:eventContent='arg'>
+        <template #eventContent="arg">
           <b>{{ arg.timeText }}</b>
           <i>{{ arg.event.title }}</i>
         </template>
